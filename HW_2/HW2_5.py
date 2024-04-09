3838dd0cfeca70786de4c814f1ef53c285609400
ArtVo <unknown.art.vo@gmail.com>
2024-04-09 11:02:28 +0300

    hw2_6

---
+++ HW_2/HW2_5.py
@@ -0,0 +1,10 @@
+message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
+search = "r"
+result = 0
+for char in message:
+    if char == search:
+        result = result + 1
+
+print(result)
+    
+   
